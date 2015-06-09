93cc70a242a6d3a67412ed62de79360da0a90a36
lmessier <leonard.messier@gmail.com>
2015-06-09 16:35:28 +0200

    (initial)

---
+++ functions.py
@@ -0,0 +1,14 @@
+import re, subprocess
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '/Users/leonard/Library/Keychains/login.keychain',
+    }
+    command = "sudo -u leonard %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+        if l.startswith('password: ')][0]
+    return re.match(r'password: "(.*)"', outtext).group(1)
